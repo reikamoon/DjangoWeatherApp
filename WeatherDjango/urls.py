e525370302edecb0a75142475980ea3e01a7fc53
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-03 14:40:15 -0700

    added authentication

--- WeatherDjango/urls.py
+++ WeatherDjango/urls.py
@@ -18,5 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('weather.urls')),
+
+    #Accounts
+    path('accounts/', include('accounts.urls')),
 ]
