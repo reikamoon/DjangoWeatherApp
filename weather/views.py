609d7be62335652c825a2cd5ede31515ba35b7bf
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-03 14:04:38 -0700

    added for loop and adjusted index

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=b071c1f6827c1026b68b3a554d84df2c'
-    city = 'San Francisco'
+    cities = City.objects.all() #return all the cities in the database
+    weather_data = []
+
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
     city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
     weather = {
         'city' : city,
@@ -12,5 +19,5 @@ def index(request):
         'description' : city_weather['weather'][0]['description'],
         'icon' : city_weather['weather'][0]['icon']
     }
-    context = {'weather' : weather}
+    context = {'weather_data' : weather_data}
     return render(request, 'weather/index.html', context) #returns the index.html template
