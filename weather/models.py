d657d2a909bb1b10736bd20964ec70a4151c8e99
Anjelica <ambrosio.anjelica@gmail.com>
2020-06-03 13:57:18 -0700

    added api key

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    def _str_(self): #show the actual city name on the dashboard
+        return self.name
+
+    class Meta: #show the plural of city as cities instead of citys
+        verbose_name_plural = 'cities'
+
+class Mood(models.Model):
+    mood = models.CharField(max_length=25)
+
+    def _str_(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'moods'
